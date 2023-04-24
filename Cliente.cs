306e3a2552177c9da8765268f3e09b00c427327e
Ana Valentim <31097110+AnaProgramando@users.noreply.github.com>
2023-04-24 19:16:42 -0300

    Create Cliente.cs

---
+++ Cliente.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_BancoVirtualDefineObtemSaldo
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+    }
+}
